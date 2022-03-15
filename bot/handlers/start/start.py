b3a02506faaa72135c2d17ddab329e04f8a4c940
Бекдаулет Казбеков <k_bekdaulet@MacBook-Air-Bekdaulet.local>
2022-03-15 17:12:04 +0600

    add users

--- bot/handlers/start/start.py
+++ bot/handlers/start/start.py
@@ -2,6 +2,7 @@ from telegram import Update
 from telegram import (
     InlineKeyboardButton, InlineKeyboardMarkup
 )
+from bot.handlers.utils.info import extract_user_data_from_update
 
 from bot.models import User
 from telegram import Update
@@ -10,8 +11,17 @@ from bot_messages.models.message import Message
 
 
 def command_start(update: Update, context: CallbackContext):
+    data = extract_user_data_from_update(update)
+    if(len(User.objects.filter(user_id=data['user_id'])) == 0):
+        user = User.objects.create(
+            user_id=data['user_id'],
+            username=data['username'],
+            first_name=data['first_name'],
+            is_blocked_bot=data['is_blocked_bot'],
+            language_code=data['language_code'],
+        )
+        user.save()
     user = User.get_user(update, context)
-
     keyboard = [
         [InlineKeyboardButton("Қазақ тілінде", callback_data="kz")],
         [InlineKeyboardButton("На русском языке", callback_data="ru")]
